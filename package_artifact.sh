24221f09a3b5272ca7c8d9056c8466b5f9586e0c
vincentvbh <vincentvbh7@gmail.com>
2025-12-18 11:44:18 +0100

    update packaging script

--- package_artifact.sh
+++ package_artifact.sh
@@ -3,7 +3,7 @@
 rm -rf artifact
 mkdir -p artifact
 cp -r BAT cycles dh GandalfFalcon GandalfFalconC GandalfMitaka hash mlkem ntru_gen randombytes symmetric artifact/
-cp Makefile README.md *.h *.c *.sh artifact/
+cp Makefile README.md LICENSE* *.h *.c *.sh artifact/
 cd artifact
 bash ./clean_up.sh
 find . -name '.git*' -exec rm -rf {} \;
